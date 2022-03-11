c620f34f663de0bcae4fd92bb45fc01221c6ad27
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-11 11:54:34 +0000

    refactor: make db primary keys autoincrement

--- src/main/resources/dao/schema.sql
+++ src/main/resources/dao/schema.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys = TRUE;
 
 CREATE TABLE "user"
 (
-    user_id INTEGER,
+    user_id INTEGER AUTOINCREMENT,
     email   VARCHAR(35) NOT NULL,
     PRIMARY KEY (user_id),
     CHECK (email LIKE '_%@_%._%')
@@ -10,7 +10,7 @@ CREATE TABLE "user"
 
 CREATE TABLE "gitlog"
 (
-    gitlog_id INTEGER,
+    gitlog_id INTEGER AUTOINCREMENT,
     user_id   INTEGER NOT NULL,
     PRIMARY KEY (gitlog_id),
     FOREIGN KEY (user_id) REFERENCES user ON UPDATE CASCADE ON DELETE CASCADE
@@ -18,9 +18,9 @@ CREATE TABLE "gitlog"
 
 CREATE TABLE "commit"
 (
-    commit_id      INTEGER,
+    commit_id      INTEGER AUTOINCREMENT,
     gitlog_id      INTEGER       NOT NULL,
-    hash           VARCHAR(40)   NOT NULL,
+    hash           VARCHAR(40)   NOT NULL UNIQUE,
     author_name    VARCHAR(30),
     author_mail    VARCHAR(35),
     header         VARCHAR(1000) NOT NULL,
@@ -34,14 +34,14 @@ CREATE TABLE "commit"
 
 CREATE TABLE "commit_type"
 (
-    commit_type_id INTEGER,
+    commit_type_id INTEGER AUTOINCREMENT,
     name           VARCHAR(10),
     PRIMARY KEY (commit_type_id)
 );
 
 CREATE TABLE "node"
 (
-    node_id   INTEGER,
+    node_id   INTEGER AUTOINCREMENT,
     commit_id INTEGER NOT NULL,
     additions INTEGER NOT NULL,
     deletions INTEGER NOT NULL,
