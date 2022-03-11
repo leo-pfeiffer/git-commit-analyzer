42d4fed0a794fa301a0abd2d2b91ea8d9bf4965b
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-11 17:59:10 +0000

    feat: sketched out actual API routes

--- src/main/scala/controller/App.scala
+++ src/main/scala/controller/App.scala
@@ -49,14 +49,14 @@ object App extends cask.MainRoutes{
 
   // GITHUB AUTH FLOW
   @cask.get("/gh-login")
-  def ghLogin() = {
+  def ghLogin(request: cask.Request): Unit = {
     val url = s"$ghBaseUri?client_id=$ghClientId&redirect_uri=$ghRedirectUri"
 
     cask.Redirect(url)
   }
 
   @cask.get("/_oauth-callback")
-  def oAuthCallback(code: String) = {
+  def oAuthCallback(code: String): Value = {
 
     val url = s"$ghBaseUri?client_id=$ghClientId&redirect_uri=$ghRedirectUri&client_secret=$ghClientSecret&code=$code"
     val r = post(url, headers = Map("accept" -> "application/json"))
@@ -68,17 +68,16 @@ object App extends cask.MainRoutes{
       println(obj("access_token"))
       obj("access_token")
     catch
-      case e: Exception => {
+      case e: Exception =>
         println(obj("error"))
         obj("error")
-      }
   }
 
   @cask.get("/delete/:user")
   def deleteUser(user: Int): String = {
     Dao.deleteUser(user) match {
       case Fail(msg) => s"This didn't work: $msg"
-      case Success(_, _) => "Yayy!"
+      case Success(_, _) => "Yay!"
     }
   }
 
@@ -86,16 +85,45 @@ object App extends cask.MainRoutes{
   def createUser(): String = {
     Dao.insertUser("leo@me.com") match {
       case Fail(msg) => s"This didn't work: $msg"
-      case Success(_, _) => "Yayy!"
+      case Success(_, _) => "Yay!"
     }
   }
 
   @cask.get("/get-user/:user")
-  def getUser(user: Int): String = {
+  def getUserById(user: Int): String = {
     Dao.findUser(user) match {
       case Fail(msg) => s"This didn't work: $msg"
       case Success(_, data: String) => s"User: $data"
+      case _ => "This shouldn't happen"
     }
+  }
+
+  // ACTUAL API ROUTES ========
+
+  @cask.get("/user/:email")
+  def getUser(email: String): String = {
+    email
+  }
+
+  @cask.postJson("/user")
+  def newUser(request: cask.Request): Value = {
+    ujson.Obj("email" -> "a@b.c")
+  }
+
+  @cask.get("/gitlog/:gitlogId")
+  def getGitlogById(gitlogId: Int): Value = {
+    ujson.Obj("gitlogId" -> gitlogId)
+  }
+
+  @cask.get("/gitlog")
+  def getAllGitlog(request: cask.Request): Value = {
+    ujson.Arr(ujson.Obj("gitlogId" -> 1))
+  }
+
+  @cask.post("/gitlog")
+  def createGitlog(request: cask.Request): Value = {
+    val log = TxtParser.main(request.text())
+    ujson.read(write(log))
   }
 
   initialize()
