4af153e8fa0380a8490de3738723d3489e0d89a8
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-12 14:12:53 +0000

    refactor: move to server directory

--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-package com.github.leopfeiffer.gitcommitanalyzer
-
-import filereader.FileReader
-import parser.TxtParser
-
-object Main extends App {
-  val content = FileReader.read("src/test/resources/test-gitlog.txt")
-  val log = TxtParser.main(content)
-}
