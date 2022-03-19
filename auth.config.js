0c212eec65e58a41a1ef193bb9a4024d506b35a0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-19 20:44:06 +0000

    feat: basic Auth0 setup

---
+++ auth.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  domain: process.env.VUE_APP_AUTH0_DOMAIN,
+  clientId: process.env.VUE_APP_AUTH0_CLIENT_ID
+}
--- src/main.js
+++ src/main.js
@@ -4,13 +4,35 @@ import router from './router'
 import Buefy from 'buefy'
 import Vuex from 'vuex'
 
-Vue.config.productionTip = false
+// Import the Auth0 configuration
+import { domain, clientId } from '../auth.config.js';
+
+console.log(domain)
+console.log(clientId)
+
+// Import the plugin here
+import { Auth0Plugin } from './auth';
 
 import 'buefy/dist/buefy.css'
 import store from "./store";
 
 Vue.use(Vuex)
 Vue.use(Buefy)
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    );
+  }
+});
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
